f869261aa6d1f8434072b99224f5e8ba71010f80
Gabriel Li <gabrielhongsheng@yahoo.com.sg>
2017-08-03 12:16:46 +0800

    AC Solutions 03August2017 and refactored HackerEarth files

---
+++ Hackerearth/Data-Structures/Advanced-Data-Structures/Fenwick-Trees/Vanya-and-array.cpp
@@ -0,0 +1,158 @@
+// Li Hong Sheng Gabriel's Competitive Programming Template v2017.10
+#include<bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tag_and_trait.hpp>
+
+//using namespace __gnu_pbds;
+using namespace std;
+
+#define ll long long
+#define mp make_pair
+#define eb emplace_back
+#define pb push_back
+#define em emplace
+#define len(s) (int) s.length()
+#define sz(v) (int) v.size()
+#define p32 pair<int,int>
+#define p64 pair<ll,ll>
+#define pdd pair<double,double>
+#define fi first
+#define se second
+#define repn(i,e) for(int i = 0; i < e; i++)
+#define repsn(i,s,e) for(int i = s; i < e; i++)
+#define rrepn(i,s) for(int i = s; i >= 0; i--)
+#define rrepsn(i,s,e) for(int i = s; i >= e; i--)
+#define v64 vector<ll>
+#define v32 vector<int>
+#define vp64 vector<p64>
+#define vp32 vector<p32>
+#define aprint(a,s) if(DRAFT) { cout << "DEBUG: "; repn(kk,s) { cout << setw(4) << right << a[kk] << " "; } cout << endl; }
+#define mprint(aa,rn,cn) if(DRAFT) { repn(i,rn) { aprint(aa[i],cn); } cout << endl; }
+#define vprint(a) if(DRAFT) { cout << "DEBUG: "; repn(kk,sz(a)) { cout << setw(4) << right << a[kk] << " "; } cout << endl; }
+#define vrprint(a,b) if(DRAFT) { cout << "DEBUG: "; repn(kk,b) { cout << setw(4) << right << a[kk] << " "; } cout << endl; }
+#define vvprint(a) if(DRAFT) repn(i,sz(a)) { cout << "DEBUG: " << i << ": "; repn(j,sz(a[i])) { cout << setw(4) << right << a[i][j] << " "; } cout << endl; }
+#define vvrprint(a,b) if(DRAFT) repn(i,b) { cout << "DEBUG: " << i << ": "; repn(j,sz(a[i])) { cout << setw(4) << right << a[i][j] << " "; } cout << endl; }
+#define value(a) if(DRAFT) { cout << "DEBUG: The value of " << #a << " is " << a << endl; }
+#define valuep(p) { if(DRAFT) cout << "DEBUG: " << "( " << p.fi << ", " << p.se << " )" << endl; }
+#define dmsg(s) { if(DRAFT) cout << "MESSAGE: " << s << endl; }
+#define inf32 INT_MAX
+#define inf64 LLONG_MAX
+#define all(a) a.begin(),a.end()
+#define UM unordered_map
+#define US unordered_set
+#define EPS 1e-10
+#define breakl "\n"
+#define MOD 1000000007
+#define lcstring(s) transform(all(s), s.begin(), ::tolower)
+#define ucstring(s) transform(all(s), s.begin(), ::toupper)
+#define lcchar(c) ((char) tolower(c))
+#define ucchar(c) ((char) toupper(c))
+#define islcchar(c) (c >= 'a' && c <= 'z')
+#define isucchar(c) (c >= 'A' && c <= 'Z')
+
+// Uncomment the include files, namespace and type to use
+//typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> AVL;
+// Usage for AVL:
+// set functions, rank and select:
+//select: cout<<*X.find_by_order(>=1)<<endl;
+//rank: cout<<X.order_of_key(>=1)<<endl;
+
+inline void scale(int x) { cout.precision(x); cout << fixed; }
+//double dist(pdd x,pdd y) { return sqrt((x.fi-y.fi)*(x.fi-y.fi)+(x.se-y.se)*(x.se-y.se)); }
+ll lpow(ll x, int y) { if(!y)return 1;ll t=lpow(x,y/2);if(y&1)return(y>0)?x*t*t:(t*t)/x;return t*t; }
+int ipow(int x, int y) { if(!y)return 1;int t=ipow(x,y/2);if(y&1)return(y>0)?x*t*t:(t*t)/x;return t*t; }
+
+void toBin(int a,int len) {
+	v32 v;
+	while(a) { v.eb(a&1), a >>= 1; }
+	while(sz(v) < len) v.eb(0);
+	reverse(all(v));
+	for(auto p : v) cout << p; cout << endl;
+}
+
+void split(vector<string> &tmp,string &line) {
+	stringstream ss(line); string s; tmp.clear();
+	while(ss >> s) tmp.eb(s);
+}
+
+inline void fastio(int debug) {
+	if(debug) {
+		cout << "DEBUGGING MODE..." << endl;
+		freopen("in","r",stdin);
+	} else {
+		ios_base::sync_with_stdio(false), cin.tie(0);
+	}
+}
+
+////////////////////////////////////// End of Template /////////////////////////////////////////////
+
+bool DRAFT = 1; // DO NOT REMOVE THIS LINE
+
+#define MAX_N 1000005
+
+// variables for fenwick tree
+int FT[MAX_N], n;
+int r[MAX_N],d[MAX_N],a[MAX_N],v[MAX_N],o[MAX_N],k;
+ll ans;
+
+int queryFT(int i) {
+	int c = i;
+	int ans = 0;
+	while(c >= 1) {
+		ans += FT[c];
+		c -= c & -c;
+	}
+	return ans;
+}
+
+void updateFT(int i,int d) {
+	int c = i;
+	while(c <= n) {
+		FT[c] += d;
+		c += c & -c;
+	}
+}
+
+bool cmp(int i,int j) {
+	if(r[i] > r[j])
+		return true;
+	else if(r[i] == r[j])
+		return i < j;
+	return false;
+}
+
+int main(void) {
+	fastio(0);
+	cin >> n >> k;
+	repsn(i,1,n+1) {
+		cin >> a[i];
+		d[i] = a[i];
+	}
+	sort(d+1,d+n+1);
+	repsn(i,1,n+1) {
+		r[i] = lower_bound(d+1,d+n+1,a[i])-d;
+		o[i] = i;
+	}
+	sort(o+1,o+n+1,cmp);
+	repsn(i,1,n+1) {
+		int t = r[o[i]]; bool f = 0;
+		while(i <= n && r[o[i]] == t) {
+			f = 1;
+			v[o[i]] = queryFT(n)-queryFT(o[i]);
+			updateFT(o[i++],1);
+		}
+		i -= f;
+	}
+	sort(v+1,v+n+1);
+	int lo = 1, hi = n;
+	while(lo < hi) {
+		if(v[lo]+v[hi] >= k) {
+			ans += hi - lo;
+			hi--;
+		} else {
+			lo++;
+		}
+	}
+	cout << ans << breakl;
+	return 0;
+}
