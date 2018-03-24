4bb6e60545324245105b23fe459c5262389430f5
Gabriel Li <gabrielhongsheng@yahoo.com.sg>
2018-03-24 16:27:09 +0800

    Update visualgo.cpp

--- Kattis/Visualgo.cpp
+++ Kattis/Visualgo.cpp
@@ -2,52 +2,53 @@
 
 using namespace std;
 
-const int maxn = 1e5 + 5;
-int n, f[maxn], a;
+const int maxn = 1e4 + 5;
+const int oo = (1LL << 31) - 1;
 
-int qr(int i) {
-	int ans = 0;
-	while(i) {
-		ans += f[i];
-		i -= (i & -i);;
-	}
-	return ans;
-}
+priority_queue<pair<int, int>, vector< pair<int, int> >, greater< pair<int, int> > > pq;
+int n, m, u, v, s, e, w, D[maxn];
+vector< pair<int, int> > G[maxn];
+map<int, int> C[maxn];
 
-void upd(int i, int v) {
-	while(i < maxn) {
-		f[i] += v;
-		i += (i & -i);
-	}
-}
-
-int select(int x) {
-	int cur = 0; x--;
-	int hi = (int) log2(maxn - 1);
-	for(int k = hi; k >= 0; k--) {
-		int p = cur + (1 << k);
-		if(p < maxn && f[p] <= x) {
-			x -= f[p];
-			cur += (1 << k);
+int d() {
+	pq.push({0, s});
+	fill(D, D + n, oo);
+	D[s] = 0;
+	C[s][0] = 1;
+	
+	while((int) pq.size() > 0) {
+		auto p = pq.top();
+		pq.pop();
+		int uw = p.first, u = p.second;
+		if(u == e) {
+			break;
+		} else if(uw > D[u]) {
+			continue;
+		}
+		
+		for(auto vi : G[u]) {
+			int w = vi.first, v = vi.second;
+			if(D[u] + w <= D[v]) {
+				C[v][D[u] + w] += C[u][D[u]];
+				if(D[u] + w < D[v]) {
+					pq.push({D[u] + w, v});
+				}
+				D[v] = D[u] + w;
+			}
 		}
 	}
-	return cur + 1;
+	return C[e][D[e]];
 }
 
 int main() {
-	ios::sync_with_stdio(false);
+	ios::sync_with_stdio(0);
 	cin.tie(0);
-	cout.precision(1);
-	cout << fixed;
-	cin >> n;
-	for(int i = 1; i <= n; i++) {
-		cin >> a;
-		upd(++a, 1);
-		if(i&1) {
-			cout << 1.0 * (select(i/2 + 1) - 1) << "\n";
-		} else {
-			cout << (select(i/2) - 1 + select(i/2 + 1) - 1) / 2.0 << "\n";
-		}
+	cin >> n >> m;
+	for(int i = 0; i < m; i++) {
+		cin >> u >> v >> w;
+		G[u].push_back({w, v});
 	}
+	cin >> s >> e;
+	cout << d() << endl;
 	return 0;
 }
